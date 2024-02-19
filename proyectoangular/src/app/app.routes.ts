14a942899abc9e54e5ea66b6b205950720c3a945
jaime-yamid <139189571+jaime-yamid@users.noreply.github.com>
2024-02-19 02:14:37 -0500

    con formulario

--- proyectoangular/src/app/app.routes.ts
+++ proyectoangular/src/app/app.routes.ts
@@ -2,21 +2,28 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
 import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 export const routes: Routes = [
     {
-        path: '',  // cyal es el path
+        path: '#',  // cyal es el path
           title: 'inicio',// el nombre de la pagina
           component: InicioComponent,   //componente como tal
 },
 {
     path: 'servicio',  // cyal es el path
-    title: 'Servicios',// el nombre de la pagina
+    title: 'Servicios',// el nombre de la pagina o arriba 
     component: ServiciosComponent,   //componente como tal
 },
 
 {
     path: 'acercade',  // cyal es el path
-    title: 'Quienes somos',// el nombre de la pagina
+    title: 'Quienes somos',// el nombre de la pagina arriba
     component: AcercaDeComponent,   //componente como tal
+    
 },
+{
+    path: 'contacto',  // cyal es el path
+    title: 'Contactos',// el nombre de la pagina
+    component: ContactoComponent,   //componente como tal
+}
 ];
--- proyectoangular/src/app/shared/header/header.component.ts
+++ proyectoangular/src/app/shared/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ContadorComponent } from "../../components/contador/contador.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
-    imports: [ContadorComponent]
+    imports: [ContadorComponent, RouterLink]
 })
 export class HeaderComponent {
   contador:number = 0;
