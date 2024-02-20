f6cb6c4187c4ec2e99f2591c65ca925e985a68cb
jaime-yamid <139189571+jaime-yamid@users.noreply.github.com>
2024-02-19 23:28:03 -0500

    con formulario mas del video

--- proyectoangular/src/app/pages/contacto/contacto.component.ts
+++ proyectoangular/src/app/pages/contacto/contacto.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+  contactoForm =  new FormGroup ({
 
+      nombre: new FormControl(''),    
+      email: new FormControl(''), 
+      mensaje: new FormControl('')             /*colocamos los nombres de los fomrularios tiene un grupo de controles cada control es el input*/
+  });
+
+  enviarContanto(){
+    console.log('enviar fomrulario');
+  }
 }
